26da39d701d792f0561165abb081c24916ba4044
Kate Kryvopustova <k.kryvopustova@gmail.com>
2021-05-21 22:31:44 +0300

    create simple chat with socket.io

---
+++ server.js
@@ -0,0 +1,26 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/index.css', (req, res) => {
+  res.sendFile(__dirname + '/index.css');
+});
+
+app.get('/index.js', (req, res) => {
+  res.sendFile(__dirname + '/index.js');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
